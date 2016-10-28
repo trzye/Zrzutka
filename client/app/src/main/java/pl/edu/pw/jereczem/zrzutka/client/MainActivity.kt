12af5e92b74a91ecdfb04a5e4f693dbf6a0b2510
trzye <jereczem@gmail.com>
2016-10-28 20:03:53 +0200

    Adding ContributionEditFragment.java

--- client/app/src/main/java/pl/edu/pw/jereczem/zrzutka/client/MainActivity.kt
+++ client/app/src/main/java/pl/edu/pw/jereczem/zrzutka/client/MainActivity.kt
@@ -1,5 +1,7 @@
 package pl.edu.pw.jereczem.zrzutka.client
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.NavigationView
@@ -9,15 +11,18 @@ import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.util.Log
+import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
-import pl.edu.pw.jereczem.zrzutka.client.model.contribution.Contribution
-import pl.edu.pw.jereczem.zrzutka.client.model.contribution.Contributor
-import pl.edu.pw.jereczem.zrzutka.client.model.contribution.Purchase
-import pl.edu.pw.jereczem.zrzutka.client.model.friend.Friend
+import android.view.WindowManager
+import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.squareup.timessquare.CalendarPickerView
 import pl.edu.pw.jereczem.zrzutka.client.modelaccess.DATABASE_FILENAME
 import pl.edu.pw.jereczem.zrzutka.client.modelaccess.DatabaseService
+import java.util.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -33,10 +38,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         dbService = DatabaseService(this)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab!!.setOnClickListener {
-            view ->
-            Snackbar.make(view, sandbox().toString(), Snackbar.LENGTH_LONG).setAction("Action", null).show()
-        }
+        fab!!.setOnClickListener { createContributionEditDialog().show() }
+
 
         val drawer = findViewById(R.id.drawer_layout) as DrawerLayout?
         val toggle = ActionBarDrawerToggle(
@@ -48,25 +51,46 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         navigationView!!.setNavigationItemSelectedListener(this)
     }
 
-    private fun sandbox(){
-
-        val contribution =  Contribution("Urodziny")
-        val friend = Friend("Andrzej")
-        val contributor = Contributor(friend, contribution)
-        val purchase = Purchase("Prezent", 10.0, contribution)
-        contribution.addContributor(contributor)
-        contribution.addPurchase(purchase)
-
-        dbService.contributorDao.create(contribution.contributors)
-        dbService.friendDao.create(contribution.contributors.flatMap { x -> listOf(x.friend) })
-        dbService.purchaseDao.create(contribution.purchases)
-        dbService.chargeDao.create(contribution.purchases.flatMap(Purchase::charges))
-        dbService.contributionDao.create(contribution)
-
-        contribution.purchases.forEach { c -> dbService.purchaseDao.createIfNotExists(c) }
-
-        val c = dbService.contributionDao.queryForAll().first()
-        Log.d("I/Zrzutka", "$c \n {${c.contributors} \n ${c.purchases} \n ${c.purchases.first().charges}")
+    private fun createContributionEditDialog() : Dialog {
+        val dialogView = layoutInflater.inflate(R.layout.fragment_contribution_edit, null)
+        val dialog = AlertDialog.Builder(this).apply {
+            setView(dialogView)
+            setCancelable(false)
+            setOnKeyListener { dialogInterface, i, keyEvent ->
+                if ((keyEvent.keyCode == KeyEvent.KEYCODE_BACK) && (keyEvent.action == KeyEvent.ACTION_UP)) {
+                    AlertDialog.Builder(this.context).apply {
+                        setMessage("Jesteś pewny?")
+                        setPositiveButton("TAK", { di, i -> dialogInterface.dismiss() })
+                        setNegativeButton("NIE", { di, i -> di.dismiss() })
+                    }.show()
+                }
+                true
+            }
+            setPositiveButton("OK", { dialogInterface, i -> })
+        }.create()
+        dialog.setOnShowListener {
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                val editText = dialogView.findViewById(R.id.editText) as TextView
+                if (editText.text.isEmpty())
+                    Snackbar.make(dialogView, "Pusty tytuł", Snackbar.LENGTH_SHORT).setAction("Action", null).apply {
+                        getView().setBackgroundColor(getColor(R.color.colorError))
+                    }.show()
+                else
+                    dialog.dismiss()
+            }
+        }
+        val calendarView = dialogView.findViewById(R.id.calendar_view) as CalendarPickerView
+        val cMin = Calendar.getInstance().apply {
+            time = Date()
+            add(Calendar.YEAR, -5)
+        }
+        val cMax = Calendar.getInstance().apply {
+            time = Date()
+            add(Calendar.YEAR, 5)
+        }
+        calendarView.init(cMin.time, cMax.time).withSelectedDate(Date()).inMode(CalendarPickerView.SelectionMode.RANGE)
+        dialog.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        return dialog
     }
 
     override fun onBackPressed() {
@@ -119,9 +143,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val drawer = findViewById(R.id.drawer_layout) as DrawerLayout?
         drawer!!.closeDrawer(GravityCompat.START)
-
-        val con = Contribution(title = "title")
-        con.title = "10"
 
         return true
     }
