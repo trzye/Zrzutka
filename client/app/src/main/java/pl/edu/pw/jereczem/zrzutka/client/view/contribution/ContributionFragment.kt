a3fcc1456878082bd4ebbe455c8ef3f9440a7ff4
trzye <jereczem@gmail.com>
2016-10-30 18:36:14 +0100

    Yup, the feel I want. For future me: NO TABHOST! PagerAdapter + PageLayout

--- client/app/src/main/java/pl/edu/pw/jereczem/zrzutka/client/view/contribution/ContributionFragment.kt
+++ client/app/src/main/java/pl/edu/pw/jereczem/zrzutka/client/view/contribution/ContributionFragment.kt
@@ -1,15 +1,12 @@
 package pl.edu.pw.jereczem.zrzutka.client
 
-import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
-import android.support.v4.content.ContextCompat
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TabHost
-import android.widget.TextView
 
 class ContributionFragment : Fragment(){
 
@@ -19,46 +16,33 @@ class ContributionFragment : Fragment(){
         val summaryLabel = view.resources.getString(R.string.tab_summary)
         val purchasesLabel = view.resources.getString(R.string.tab_purchases)
 
-        val tabHost = (view.findViewById(R.id.contributionTabHost) as TabHost).apply {
-            setup()
-        }
-//
-//        tabHost.addTab(summaryLabel, R.id.summaryTab)
-//        tabHost.addTab(contributorsLabel, R.id.contributorsTab)
-//        tabHost.addTab(purchasesLabel, R.id.purchasesTab)
+        val tabLayout = view.findViewById(R.id.tabLayout) as TabLayout
+        tabLayout.addTab(tabLayout.newTab().setText(summaryLabel))
+        tabLayout.addTab(tabLayout.newTab().setText(purchasesLabel))
+        tabLayout.addTab(tabLayout.newTab().setText(contributorsLabel))
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL;
 
-//        val x = createTabView(view.context, "Test")
+        val viewPager = view.findViewById(R.id.viewPager) as ViewPager
+        val pagerAdapter = ContributionPagerAdapter(fragmentManager)
 
-        setupTab(tabHost, view.findViewById(R.id.contributorsTab), contributorsLabel, android.R.drawable.ic_dialog_email)
-        setupTab(tabHost, view.findViewById(R.id.summaryTab), summaryLabel, android.R.drawable.ic_dialog_email)
-        setupTab(tabHost, view.findViewById(R.id.purchasesTab), purchasesLabel, android.R.drawable.ic_dialog_email)
+        viewPager.adapter = pagerAdapter
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
 
+        tabLayout.setOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+            }
+
+        })
 
         return view
     }
 
-    private fun TabHost.addTab(contributorsLabel: String, layoutId: Int) {
-        addTab(
-                newTabSpec(contributorsLabel).apply {
-                    setContent(layoutId)
-                    setIndicator("", ContextCompat.getDrawable(activity, android.R.drawable.ic_dialog_email))
-                }
-        )
-    }
-
-    private fun setupTab(mTabHost: TabHost, view: View, tag: String, icon: Int) {
-        val tabview = createTabView(mTabHost.context, tag, icon)
-        val setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent { view }
-        mTabHost.addTab(setContent)
-    }
-
-    private fun createTabView(context: Context, text: String, icon: Int): View {
-        val view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null)
-        val tv = view.findViewById(R.id.tabsText) as TextView
-        val im = view.findViewById(R.id.tabsImage) as ImageView
-        im.setImageDrawable(ContextCompat.getDrawable(context, icon))
-        tv.text = text
-        return view
-    }
 
 }
